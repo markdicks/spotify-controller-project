5a5040b69e63a0260d2ddebd32e7f3a1c44e58b5
markdicks <markdicks03@gmail.com>
2023-12-05 14:10:55 +0200

    Getting tokens for access to twitch Making some sort of design for interface

--- spotify-controller-project/Form1.Designer.cs
+++ spotify-controller-project/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace spotify_controller_project
 {
-    partial class Form1
+    partial class TwitchApp
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            connectButton = new Button();
+            live_chat_link = new TextBox();
+            live_link_label = new Label();
+            SuspendLayout();
+            // 
+            // connectButton start the code
+            // 
+            connectButton.Location = new Point(699, 283);
+            connectButton.Name = "connectButton";
+            connectButton.Size = new Size(100, 27);
+            connectButton.TabIndex = 0;
+            connectButton.Text = "Connect";
+            connectButton.UseVisualStyleBackColor = true;
+            connectButton.Click += button1_Click;
+            // 
+            // live_chat_link usr input
+            // 
+            live_chat_link.Location = new Point(213, 90);
+            live_chat_link.Name = "live_chat_link";
+            live_chat_link.Size = new Size(394, 23);
+            live_chat_link.TabIndex = 1;
+            // 
+            // live_link_label description
+            // 
+            live_link_label.AutoSize = true;
+            live_link_label.Location = new Point(308, 57);
+            live_link_label.Name = "live_link_label";
+            live_link_label.Size = new Size(205, 15);
+            live_link_label.TabIndex = 2;
+            live_link_label.Text = "Please enter your live stream chat link";
+            live_link_label.Click += label1_Click;
+            // 
+            // TwitchApp Form
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(811, 322);
+            Controls.Add(live_link_label);
+            Controls.Add(live_chat_link);
+            Controls.Add(connectButton);
+            Name = "TwitchApp";
+            Text = "TwitchApp";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button connectButton;
+        private TextBox live_chat_link;
+        private Label live_link_label;
     }
 }
--- spotify-controller-project/Program.cs
+++ spotify-controller-project/Program.cs
@@ -11,7 +11,7 @@ namespace spotify_controller_project
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new TwitchApp());
         }
     }
 }
