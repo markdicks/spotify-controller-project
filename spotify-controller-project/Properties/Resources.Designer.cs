a17fc6df54929243e22c9aaf2a793381dd258a21
markdicks <markdicks03@gmail.com>
2023-12-06 10:29:06 +0200

    attempt 1 assembly duplicate issues with build, try to solve

--- spotify-controller-project/Properties/Resources.Designer.cs
+++ spotify-controller-project/Properties/Resources.Designer.cs
@@ -44,7 +44,7 @@ namespace spotifycontrollerproject_properties.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("tester_properties.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("spotify-controller-project_properties.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
