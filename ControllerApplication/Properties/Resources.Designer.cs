0629fea4b9547f49251033eaea0c0412038fca99
markdicks <markdicks03@gmail.com>
2024-06-28 13:30:46 +0200

    Remove old code for improvements

--- ControllerApplication/Properties/Resources.Designer.cs
+++ ControllerApplication/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace AttempTrial.Properties
+namespace ControllerApplication.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace AttempTrial.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AttempTrial.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ControllerApplication.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- ControllerApplication/Properties/Settings.Designer.cs
+++ ControllerApplication/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace AttempTrial.Properties
+namespace ControllerApplication.Properties
 {
 
 
