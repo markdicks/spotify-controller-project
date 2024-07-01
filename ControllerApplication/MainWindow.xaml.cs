c44fccfdc3bba7503a8f8bc01f080c6e1a7baa35
Mark Dicks <117034609+markdicks@users.noreply.github.com>
2024-07-01 11:42:19 +0200

    Spotify information reworked project (#13)
    * Attempt at reading spotify info
    
    * .net framework
    
    * Remove old code for improvements
    
    * Remove old code
    
    * Remove old login logic
    
    * Cleaning up

---
+++ ControllerApplication/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Windows;
+using Windows.Storage.Pickers;
+
+namespace ControllerApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        var picker = new Windows.Storage.Pickers.FileOpenPicker();
+
+        private GlobalSystemMediaTransportControlsSessionManager _mediaManager;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeMediaManager();
+        }
+
+        private async void InitializeMediaManager()
+        {
+            try
+            {
+                _mediaManager = await GlobalSystemMediaTransportControlsSessionManager.RequestAsync();
+                _mediaManager.CurrentSessionChanged += MediaManager_CurrentSessionChanged;
+                UpdateCurrentSong();
+            }
+            catch (Exception ex)
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    CurrentSongTextBlock.Text = $"Error: {ex.Message}";
+                });
+            }
+        }
+
+        private void MediaManager_CurrentSessionChanged(GlobalSystemMediaTransportControlsSessionManager sender, CurrentSessionChangedEventArgs args)
+        {
+            UpdateCurrentSong();
+        }
+
+        private async void UpdateCurrentSong()
+        {
+            var currentSession = _mediaManager.GetCurrentSession();
+            if (currentSession != null)
+            {
+                var mediaProperties = await currentSession.TryGetMediaPropertiesAsync();
+                Dispatcher.Invoke(() =>
+                {
+                    CurrentSongTextBlock.Text = $"Currently Playing: {mediaProperties.Title} by {mediaProperties.Artist}";
+                });
+            }
+            else
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    CurrentSongTextBlock.Text = "Currently Playing: None";
+                });
+            }
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            var currentSession = _mediaManager.GetCurrentSession();
+            currentSession?.TryPlayAsync();
+        }
+
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            var currentSession = _mediaManager.GetCurrentSession();
+            currentSession?.TryPauseAsync();
+        }
+
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+            var currentSession = _mediaManager.GetCurrentSession();
+            currentSession?.TrySkipNextAsync();
+        }
+
+        private void PreviousButton_Click(object sender, RoutedEventArgs e)
+        {
+            var currentSession = _mediaManager.GetCurrentSession();
+            currentSession?.TrySkipPreviousAsync();
+        }
+
+        private void VolumeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            // Volume control logic
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Login functionality is not needed for local media control.");
+        }
+    }
+}
